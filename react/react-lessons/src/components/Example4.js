35cb1161f4582c1bb352e54ad64b129c2b13f6a3
ThazinCoding <thazin799073@gmail.com>
2025-05-29 15:01:50 +0630

    react

---
+++ react/react-lessons/src/components/Example4.js
@@ -0,0 +1,15 @@
+"use client";
+
+import React,{useState} from "react";
+
+export default function Person(){
+    const [name,setName]=useState("Mg Kyaw Min");
+    const [date,setDate]=useState(new Date());
+    return(
+        <div>
+            <h1>State in Functional Component</h1>
+            <div>{name}</div>
+            <div>{date.toString()}</div>
+        </div>
+    );
+}
